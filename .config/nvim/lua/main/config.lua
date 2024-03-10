349fcb531ea34915a18672b16b044fa2c8b70eee
MikeHorn-git <123373126+MikeHorn-git@users.noreply.github.com>
2024-03-10 22:02:31 +0000

    Add files via upload

---
+++ .config/nvim/lua/main/config.lua
@@ -0,0 +1,8 @@
+vim.cmd('set number')						-- Show line number at startup
+vim.cmd('set relativenumber')					-- Show relative line number, execpt for the current line
+
+vim.o.tabstop = 4						-- Set tab value of 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+				
+vim.api.nvim_set_option("clipboard", "unnamed")			-- Allow copy text in nvim to the OS buffer
---
+++ .config/nvim/lua/main/keymaps.lua
@@ -0,0 +1,21 @@
+vim.g.mapleader = " "						-- Set <Space> as the leader key
+
+-- Commands
+vim.keymap.set("n", "<leader>g", vim.cmd.Git)			-- Fugitive [Git]
+vim.keymap.set("n", "<leader>l", vim.cmd.Lazy)			-- Lazy [PackageManager Plugins]
+vim.keymap.set("n", "<leader>m", vim.cmd.Mason)			-- Mason [PackageManager LSP]
+vim.keymap.set("n", "<leader>p", vim.cmd.VimBeGood)		-- VimBeGood [Training]
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)	-- Undotree [History Visualizer]
+vim.keymap.set("n", "<leader>w", vim.cmd.WhichKey)		-- Which-key [Key Bindings]
+vim.keymap.set("n", "<leader>z", vim.cmd.ZenMode)		-- Zen-mode [Distraction free]
+
+-- Motion
+vim.keymap.set("n", "<C-d>", "<C-d>zz")				-- Center the screen when scroll down
+vim.keymap.set("n", "<C-u>", "<C-u>zz")				-- Center the screen when scroll up
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)		-- Format the current buffer according to the lsp rule
+vim.keymap.set("n", "J", "mzJ`z")				-- Join current line to the below one
+vim.keymap.set("i", "<C-c>", "<Esc>")				-- Remap <Escape> to <ctrl+c> in Insert mode
+
+-- Personnal
+vim.keymap.set("n", "<leader>n", vim.cmd.Ex)			-- Back to File Navigation mode
+vim.keymap.set("n", "<leader>cl", vim.cmd.noh)			-- Clear highlights
