f7fe7bc6629291a1ee2687cc321299784d8dc4f6
MikeHorn-git <123373126+MikeHorn-git@users.noreply.github.com>
2024-05-01 13:58:55 +0200

    Delete scripts/blackarch.sh

--- scripts/blackarch.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-sudo pacman -S binaryninja-demo binwalk burpsuite checksec dirb exiv2 exploitdb foremost gef gdb ghidra ida-free impacket john ltrace lynis networkminer nuclei python-pipx radare2 rkhunter rsactftool seclists sleuthkit sqlmap strace trivy volatility3 wireshark-qt xsstrike yara zaproxy
-yay -S python2-volatility-git
