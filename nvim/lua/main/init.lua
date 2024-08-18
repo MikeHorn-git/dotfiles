7ba88c8914805f1ee7f0f04c41732889df9f9a50
MikeHorn-git <MikeHornProton@proton.me>
2024-08-18 23:06:52 +0200

    Add files via upload

---
+++ nvim/lua/main/init.lua
@@ -0,0 +1,3 @@
+require("main.config")
+require("main.keymaps")
+require("main.lazy")
---
+++ nvim/lua/main/lazy.lua
@@ -0,0 +1,62 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Alphabet plugin name order
+require("lazy").setup({
+	{
+    		"ThePrimeagen/harpoon",
+    		branch = "harpoon2",
+    		dependencies = { "nvim-lua/plenary.nvim" }
+	},
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = 'v3.x',
+		lazy = true,
+		config = false,
+	},
+	{
+	    'nvim-lualine/lualine.nvim',
+	    dependencies = { 'nvim-tree/nvim-web-devicons' }
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = { "L3MON4D3/LuaSnip" }
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = { "hrsh7th/cmp-nvim-lsp" }
+	},
+	"nvim-treesitter/nvim-treesitter",
+	"williamboman/mason-lspconfig.nvim",
+	"williamboman/mason.nvim",
+	{
+		"nvim-telescope/telescope.nvim", tag = '0.1.5',
+		dependencies = { "nvim-lua/plenary.nvim" }
+	},
+	{
+ 		"folke/trouble.nvim",
+ 		dependencies = { "nvim-tree/nvim-web-devicons" }
+	},
+	"folke/tokyonight.nvim",
+	"mbbill/undotree",
+	"ThePrimeagen/vim-be-good",
+	"tpope/vim-fugitive",
+	{
+		"folke/which-key.nvim",
+  		init = function()
+    			vim.o.timeout = true
+    			vim.o.timeoutlen = 300
+  		end,
+	},
+	"folke/zen-mode.nvim"
+})
