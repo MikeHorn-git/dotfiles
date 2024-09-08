b18a8fef36257a482ab78e83dc3371815ace07a6
MikeHorn-git <MikeHornProton@proton.me>
2024-09-08 09:13:45 +0000

    Fix utils.sh

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -23,12 +23,19 @@ echo 26849980b35a42e6e192c6d9ed8c46f0d6d06047 strap.sh | sha1sum -c
 chmod +x strap.sh
 sudo ./strap.sh
 rm ./strap.sh
+sudo pacman -Syyu --noconfirm
 
 echo "[+] Configure virt-manager"
-sudo pacman -S --neded dmidecode qemu-base virt-manager
+sudo pacman -S --needed --noconfirm dmidecode dnsmasq qemu-base virt-manager
 sudo systemctl enable --now libvirtd.service
 sudo sed -i "/#unix_sock_group = \"libvirt\"/cunix_sock_group = \"libvirt\"" /etc/libvirt/libvirtd.conf
 sudo sed -i "/#unix_sock_ro_perms = \"0777\"/cunix_sock_ro_perms = \"0777\"" /etc/libvirt/libvirtd.conf
+sudo usermod -a -G libvirt "$(whoami)"
+sudo systemctl restart libvirtd.service
+
+echo "[+] Configure thinkfan"
+yay -S thinkfan
+sudo systemctl enable --now thinkfan.service
 
 echo "[+] Install oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
