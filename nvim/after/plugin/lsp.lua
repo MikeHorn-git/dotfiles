a8703a198f5fba0fb934f4ab490552e11fefa142
MikeHorn-git <123373126+MikeHorn-git@users.noreply.github.com>
2025-08-03 11:10:19 +0200

    Update lsp.lua

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -2,8 +2,10 @@ require("mason").setup()
 require("mason-lspconfig").setup({
 	ensure_installed = {
 		"clangd",
+		"lua_ls",
 		"nil_ls",
 		"pylsp",
+		"ruff",
 		"rust_analyzer",
 	},
 	automatic_enable = true,
